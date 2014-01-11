0c8260dca0269a797285243880e3ce7cddd8badf
ONLYUSEmeSPOONS <daniel.senekal242@gmail.com>
2014-01-10 16:47:52 -0800

    Moving snake - wooot!

--- src/snakegame/Snake.java
+++ src/snakegame/Snake.java
@@ -4,10 +4,97 @@
  */
 package snakegame;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import static snakegame.Direction.UP;
+
 /**
  *
  * @author Daniel
  */
 public class Snake {
     
+    public void move(){
+        Point newHead = new Point();
+        
+        switch (direction){
+            case UP:
+                newHead.x = getHead().x;
+                newHead.y = getHead().y - 1;
+                break;
+
+            case DOWN:
+                newHead.x = getHead().x;
+                newHead.y = getHead().y + 1;
+                break;
+            
+            case LEFT:
+                newHead.x = getHead().x - 1;
+                newHead.y = getHead().y;
+                break;
+            
+            case RIGHT:
+                newHead.x = getHead().x + 1;
+                newHead.y = getHead().y;
+                break;
+        }
+        
+        body.add(0, newHead);
+        body.remove(body.size() - 1);
+        
+    }
+    
+    public Point getHead(){
+        return body.get(0);
+    }
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Properties">
+    private ArrayList<Point> body = new ArrayList<Point>();
+    private Direction direction = Direction.RIGHT;
+    private int growthCount = 0;
+    
+    /**
+     * @return the body
+     */
+    public ArrayList<Point> getBody() {
+        return body;
+    }
+    
+    /**
+     * @param body the body to set
+     */
+    public void setBody(ArrayList<Point> body) {
+        this.body = body;
+    }
+    
+    /**
+     * @return the growthCount
+     */
+    public int getGrowthCount() {
+        return growthCount;
+    }
+    
+    /**
+     * @param growthCount the growthCount to set
+     */
+    public void setGrowthCount(int growthCount) {
+        this.growthCount = growthCount;
+    }
+    
+    /**
+     * @return the direction
+     */
+    public Direction getDirection() {
+        return direction;
+    }
+
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    //</editor-fold>
+
 }
--- src/snakegame/SnakeEnvironment.java
+++ src/snakegame/SnakeEnvironment.java
@@ -7,7 +7,9 @@ package snakegame;
 import environment.Environment;
 import environment.Grid;
 import image.ResourceTools;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
@@ -16,8 +18,10 @@ import java.awt.event.MouseEvent;
  * @author Daniel
  */
 class SnakeEnvironment extends Environment {
-    Grid grid;
-  
+    private Grid grid;
+    private Snake snake;
+    private int defaultDelay = 4;
+    private int delay = defaultDelay;
 
     @Override
     public void initializeEnvironment() {
@@ -27,17 +31,37 @@ class SnakeEnvironment extends Environment {
         grid.setColumns(35);
         grid.setRows(30);
         
+        snake = new Snake();
+        snake.getBody().add(new Point(5, 5));
+        snake.getBody().add(new Point(4, 5));
+        snake.getBody().add(new Point(4, 4));
+        
         this.setBackground(ResourceTools.loadImageFromResource("resources/elephants.jpg"));
     }
 
     @Override
     public void timerTaskHandler() {
-
+        if (snake != null) {
+            if (delay <= 0) {
+                snake.move();
+                delay = defaultDelay;
+            } else {
+                delay--;
+            }
+        }
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
-
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            snake.setDirection(Direction.UP);
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            snake.setDirection(Direction.DOWN);
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            snake.setDirection(Direction.LEFT);
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            snake.setDirection(Direction.RIGHT);
+        }  
     }
 
     @Override
@@ -54,6 +78,13 @@ class SnakeEnvironment extends Environment {
     public void paintEnvironment(Graphics graphics) {
         if (grid != null) {
             grid.paintComponent(graphics);
+        }
+        
+        if (snake != null) {
+            graphics.setColor(Color.GREEN);
+            for (int i = 0; i < snake.getBody().size(); i++) {
+                graphics.fillRect(grid.getCellPosition(snake.getBody().get(i)).x, grid.getCellPosition(snake.getBody().get(i)).y, grid.getCellWidth(), grid.getCellHeight());
+            }
         }
     }
     
