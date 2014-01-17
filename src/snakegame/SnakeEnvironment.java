5f55b942c71d7e55f24e52cab1b4b8292ff1f73a
ONLYUSEmeSPOONS <daniel.senekal242@gmail.com>
2014-01-17 08:23:21 -0800

    New Commit

--- src/snakegame/SnakeEnvironment.java
+++ src/snakegame/SnakeEnvironment.java
@@ -5,38 +5,43 @@
 package snakegame;
 
 import environment.Environment;
+import environment.GraphicsPalette;
 import environment.Grid;
 import image.ResourceTools;
-import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.Color;
 
 /**
  *
  * @author Daniel
  */
 class SnakeEnvironment extends Environment {
+
     private Grid grid;
     private Snake snake;
-    private int defaultDelay = 2;
+    private int defaultDelay = 0;
     private int delay = defaultDelay;
+    private int score = 0;
 
     @Override
     public void initializeEnvironment() {
         grid = new Grid();
+        grid.setColor(Color.GREEN);
         grid.getPosition().x = 50;
         grid.getPosition().y = 100;
-        grid.setColumns(35);
-        grid.setRows(30);
-        
+        grid.setColumns(90);
+        grid.setRows(45);
+
         snake = new Snake();
         snake.getBody().add(new Point(5, 5));
         snake.getBody().add(new Point(4, 5));
         snake.getBody().add(new Point(4, 4));
-        
-        this.setBackground(ResourceTools.loadImageFromResource("resources/elephants.jpg"));
+
+        this.setBackground(ResourceTools.loadImageFromResource("resources/autumn.jpg"));
     }
 
     @Override
@@ -49,6 +54,23 @@ class SnakeEnvironment extends Environment {
                 delay--;
             }
         }
+          if (snake.getDirection() == Direction.RIGHT) {
+                if (snake.getHead().x >= this.grid.getColumns()) {
+                    snake.getHead().x = 0 ;
+                }
+            } else if (snake.getDirection() == Direction.LEFT) {
+                if (snake.getHead().x <=-1) {
+                    snake.getHead().x = this.grid.getColumns()-1;
+                }
+            } else if (snake.getDirection() == Direction.UP) {
+                if (snake.getHead().y <= -1) {
+                    snake.getHead().y = this.grid.getRows()-1;
+                }
+            } else if (snake.getDirection() == Direction.DOWN) {
+                if (snake.getHead().y >= this.grid.getRows()) {
+                    snake.getHead().y = 0;
+                }
+            }
     }
 
     @Override
@@ -63,17 +85,19 @@ class SnakeEnvironment extends Environment {
             snake.setDirection(Direction.RIGHT);
         } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             snake.setGrowthCount(2);
-        }  
+        } else if (e.getKeyCode() == KeyEvent.VK_C) {
+            this.score += 10;
+
+
+        }
     }
 
     @Override
     public void keyReleasedHandler(KeyEvent e) {
-
     }
 
     @Override
     public void environmentMouseClicked(MouseEvent e) {
-
     }
 
     @Override
@@ -81,13 +105,19 @@ class SnakeEnvironment extends Environment {
         if (grid != null) {
             grid.paintComponent(graphics);
         }
-        
+
         if (snake != null) {
             graphics.setColor(Color.WHITE);
             for (int i = 0; i < snake.getBody().size(); i++) {
                 graphics.fillOval(grid.getCellPosition(snake.getBody().get(i)).x, grid.getCellPosition(snake.getBody().get(i)).y, grid.getCellWidth(), grid.getCellHeight());
+
             }
         }
+        graphics.setFont(new Font("Calibri ", Font.ITALIC, 50));
+        graphics.drawString("Score: " + this.score, 50, 50);
+        
+        GraphicsPalette.enterPortal(graphics, new Point(100, 100), new Point(this.grid.getCellSize()), Color.yellow);
+        GraphicsPalette.leavePortal(graphics, new Point(500, 500), new Point(this.grid.getCellSize() ), Color.yellow);
+        GraphicsPalette.drawPoisonBottle(graphics, new Point(300, 300),new Point(this.grid.getCellSize()), Color.yellow);
     }
-    
 }
